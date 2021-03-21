7baaf03fcfc9a9c96b8c62229d5e88da63fc09ac
whoogle <79408469+whoogle@users.noreply.github.com>
2021-03-21 15:16:33 +0000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+api_id = 1250139
+api_hash = "63651e099ce2619f65eadf8341073cf5"
+sudo_chat_id = -1001309971572
+owner_id = 1119038066
+
+
+# Arq Api, don't make changes here
+ARQ_API = "https://thearq.tech"
